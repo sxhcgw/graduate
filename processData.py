a254c0ffaf037bc49f11f09d9f7fdcb413a9c93b
gaowei <waltergao@sohu-inc.com>
2017-10-12 15:07:42 +0800

    '去重完成'

--- processData.py
+++ processData.py
@@ -4,10 +4,11 @@
 import jieba
 
 data = '木质后盖蛮喜欢的'
-path = r'file/resource/stopwords.txt'
-file_path = r'file/resource/test1.txt'
-seg_path = r'file/resource/seg_file.txt'
-res_path = r'file/resource/res.txt'
+path = r'file/resource/stopwords.txt'     #停用词表
+file_path = r'file/resource/test1.txt'    #原始数据
+seg_path = r'file/resource/seg_file.txt'  #分词结果
+res_path = r'file/resource/res.txt'       #相似度
+distinct_path = r'file/resource/distinct.txt'       #最终结果
 #seg = peg.cut(data)   #精确模式
 seg = jieba.cut(data)
 
@@ -25,24 +26,26 @@ def seg_sentence(seg, stopWords):
                 outstr += word
                 outstr += " "
     return outstr
-#获得停用词
-stopWords = getFileContent(path)
-#保存最后结果
-res = ''
-#获取用户评论
-fileContent = getFileContent(file_path)
-#分词并去除停用词
-for line in fileContent:
-    line = line.strip()
-    if line == '':
-        continue
-    tmp = jieba.cut(line)
-    res += seg_sentence(tmp, stopWords)
-    res += '\n'
-#写入文件
-seg_file = open(seg_path, 'w', encoding='utf-8')
-seg_file.write(res)
-seg_file.close()
+
+def process():
+    #获得停用词
+    stopWords = getFileContent(path)
+    #保存最后结果
+    res = ''
+    #获取用户评论
+    fileContent = getFileContent(file_path)
+    #分词并去除停用词
+    for line in fileContent:
+        line = line.strip()
+        if line == '':
+            continue
+        tmp = jieba.cut(line)
+        res += seg_sentence(tmp, stopWords)
+        res += '\n'
+    #写入文件
+    seg_file = open(seg_path, 'w', encoding='utf-8')
+    seg_file.write(res)
+    seg_file.close()
 
 
 #计算tf-idf值
@@ -52,23 +55,50 @@ class MyCorpus(object):
             yield line.split()
 
 from gensim import corpora, models, similarities
-Corp = MyCorpus()
-dictionary = corpora.Dictionary(Corp)
-corpus = [dictionary.doc2bow(text) for text in Corp]
-tfidf = models.TfidfModel(corpus)
-corpus_tfidf = tfidf[corpus]
-#把评论做成索引
-query = '打电话 时有 回音 ， 买 父亲 、 十天 ， 、 '
-vec_bow = dictionary.doc2bow(query.split())
-vec_tfidf = tfidf[vec_bow]
-# for item in corpus_tfidf:
-#     print(item)
-index = similarities.MatrixSimilarity(corpus_tfidf)
-sims = index[vec_tfidf]
-similarity = list(sims)
-sim_file = open(res_path, 'w')
-for i in similarity:
-    sim_file.write(str(i) + "\n")
-sim_file.close()
 
-
+def model():
+    Corp = MyCorpus()
+    dictionary = corpora.Dictionary(Corp)
+    corpus = [dictionary.doc2bow(text) for text in Corp]
+    tfidf = models.TfidfModel(corpus)
+    corpus_tfidf = tfidf[corpus]
+    #把评论做成索引
+    # query = '打电话 时有 回音 ， 买 父亲 、 十天 ， 、 '
+    # vec_bow = dictionary.doc2bow(query.split())
+    # vec_tfidf = tfidf[vec_bow]
+    # for item in corpus_tfidf:
+    #     print(item)
+    index = similarities.MatrixSimilarity(corpus_tfidf)
+    content = getFileContent(file_path)
+    seg_content = getFileContent(seg_path)
+    i = 0
+    res_tmp = []
+    for seg_item in seg_content:
+        vec_bow = dictionary.doc2bow(seg_item.split())
+        vec_tfidf = tfidf[vec_bow]
+        sims = index[vec_tfidf]
+        similarity = list(sims)
+        j = 0
+        flag = True
+        for sim in similarity:
+            if j <= i:   #后边的不和前边的进行比较
+                j = j + 1
+                continue
+            if sim >= 0.95:
+                flag = False
+                break
+            j = j + 1
+        if flag:
+            res_tmp.append(content[i])
+        i = i + 1
+    distinct_file = open(distinct_path, 'w', encoding='utf-8')
+    for item in res_tmp:
+        distinct_file.write(item + "\n")
+    distinct_file.close()
+    # sims = index[vec_tfidf]
+    # similarity = list(sims)
+    # sim_file = open(res_path, 'w')
+    # for i in similarity:
+    #     sim_file.write(str(i) + "\n")
+    # sim_file.close()
+model()
